f0f87c1892d36fe8d3d05b191f3b248f33f01777
wdpfm <pfm9866@126.com>
2021-01-25 22:52:28 +0800

    数组工具类

---
+++ src/com/wdpfm/line/array/ArrayUtils.java
@@ -0,0 +1,49 @@
+package com.wdpfm.line.array;
+
+/**
+ * 描述:数组工具类
+ */
+public class ArrayUtils {
+    //将指定的元素插入到指定数组的指定位置上
+    //时间复杂度O(n)
+    //空间复杂度O(n)
+    public static int[] insertElement(int[] src,int index,int element){
+        int length=src.length;
+        //创建比原数组大1的新数组
+        int[] dest=new int[length+1];
+
+        //前面部分复制到新数组
+        for (int i = 0; i < index; i++) {
+            dest[i]=src[i];
+        }
+
+        //新元素复制到新数组
+        dest[index]=element;
+
+        //后面部分复制到新数组
+        for (int i = index; i < length; i++) {
+            dest[i+1]=src[i];
+        }
+        return dest;
+    }
+
+    //从数组中删除指定位置的元素
+    //时间复杂度O(n)
+    //空间复杂度O(n)
+    public static int[] removeElement(int[] src,int index){
+        int length=src.length;
+        //创建比原数组小1的新数组
+        int[] dest=new int[length-1];
+
+        //前面部分复制到新数组
+        for (int i = 0; i < index; i++) {
+            dest[i]=src[i];
+        }
+
+        //后面部分复制到新数组
+        for (int i = index; i < src.length -1; i++) {
+            dest[i]=src[i+1];
+        }
+        return dest;
+    }
+}
