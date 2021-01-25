8cca386d1e25875e2d9d0f94afa79582fe5416e6
wdpfm <pfm9866@126.com>
2021-01-25 23:33:27 +0800

    重写数组toString方法

--- src/com/wdpfm/line/array/ArrayList.java
+++ src/com/wdpfm/line/array/ArrayList.java
@@ -31,6 +31,7 @@ public class ArrayList {
     }
 
     //指定位置插入元素
+    //时间复杂度O(n)
     public void insert(int index,int e){
         if (size==data.length)
             throw new IllegalArgumentException("插入失败，数组已满");
@@ -53,5 +54,44 @@ public class ArrayList {
     //尾插
     public void insertLast(int e){
         insert(size,e);
+    }
+
+    //查询操作
+    public int get(int index){
+        if(index<0||index>=size)
+            throw new IllegalArgumentException("索引异常");
+        return data[index];
+    }
+
+    //查找操作 查找元素e所在的索引，如果元素e不存在，则返回-1
+    public int find(int e){
+        for (int i = 0; i < size; i++) {
+            if (data[i]==e)
+                return i;
+        }
+        return -1;
+    }
+
+    //判断是否存在
+    public boolean contain(int target){
+        for (int num:data){
+            if (num==target)
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb=new StringBuilder();//用于拼接字符串
+        sb.append(String.format("数组大小:%d，数组容量:%d\n",size,capacity));
+        sb.append("[");
+        for (int i = 0; i < size; i++) {
+            sb.append(data[i]);
+            if (i!=size-1)
+                sb.append(",");
+        }
+        sb.append("]");
+        return sb.toString();
     }
 }
---
+++ src/com/wdpfm/line/array/ArrayListTest.java
@@ -0,0 +1,15 @@
+package com.wdpfm.line.array;
+
+/**
+ * 描述:测试类
+ */
+public class ArrayListTest {
+    public static void main(String[] args) {
+        ArrayList arrayList=new ArrayList();
+        System.out.println(arrayList.isEmpty());
+        arrayList.insertFirst(1);
+        arrayList.insertLast(2);
+        System.out.println(arrayList.isEmpty());
+        System.out.println(arrayList);
+    }
+}
