e6ff1fde24a74fe01b431c95584be45951abbc74
wdpfm <pfm9866@126.com>
2021-01-26 16:39:31 +0800

    链表查询操作

--- src/com/wdpfm/line/linkedlist/LinkedList.java
+++ src/com/wdpfm/line/linkedlist/LinkedList.java
@@ -46,4 +46,24 @@ public class LinkedList<E> {
     public boolean isEmpty(){
         return size==0;
     }
+
+    //查询指定索引的节点值
+    public E get(int index){
+        //检查index合法性
+        if (index<0||index>=size)
+            throw new IllegalArgumentException("索引异常");
+        Node current=head;
+        for (int i = 0; i < index; i++) {
+            current=current.next;
+        }
+        return current.e;
+    }
+
+    public E getFirst(){
+        return get(0);
+    }
+
+    public E getLast(){
+        return get(size-1);
+    }
 }
